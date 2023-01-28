80bc37c4d503b2f8872a0637468c76e3abba8823
Marien Mupenda <marien.mupenda@gmail.com>
2023-01-28 16:23:29 +0200

    add name on error

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,13 +1,15 @@
 @props(['label'])
 @php
     $model = $attributes['name'] ?? $attributes->wire('model')->value();
-    if ($errors->has($model)) {
+    if ($errors) {
+        dd($errors);
         $error = $errors->first($model);
         $error_class = 'is-invalid';
     } else {
         $error = '';
         $error_class = '';
     }
+
 @endphp
 @include('form::components.label')
 <input {!! $attributes->merge(['class' => 'form-control '.$error_class]) !!}>
