44ca591879b376af889a6b2f6f52b59fef7cd025
Marien Mupenda <marien.mupenda@gmail.com>
2023-02-01 22:09:30 +0200

    add simple select

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,48 +1,25 @@
-@props(['label'=>null,'placeholder' => null,'multiple'=>false,'allowClear'=>false])
+@props(['label','placeholder' => true,'placeholderDisabled' => false,'placeholderText'=>'-- Sélectionner --'])
+@props(['label'=>null,'placeholder' => true,'placeholderDisabled' => false,'placeholderText'=>'-- Sélectionner --'])
 @php
-
-    if ($multiple) {
-      $attributes =  $attributes->merge(['multiple' => 'multiple']);
-    }
-         $model = $attributes['name'] ?? $attributes->wire('model')->value();
-         $select_id = SmirlTech\LaravelForm\Helpers\Helpers::modelToFucntionName($model);
-        if ($errors->has($model)) {
-            $error = $errors->first($model);
-            $error_class = 'is-invalid';
-        } else {
-            $error = '';
-            $error_class = '';
-        }
+    $model = $attributes['name'] ?? $attributes->wire('model')->value();
+   if ($errors->has($model)) {
+       $error = $errors->first($model);
+       $error_class = 'is-invalid';
+   } else {
+       $error = '';
+       $error_class = '';
+   }
 @endphp
 @include('form::components.label')
-{{--<span wire:ignore>--}}
-<select id="{{$select_id}}" {!! $attributes->merge(['class' => 'form-control form-select '.$error_class]) !!}>
+<select {!! $attributes->merge(['class' => 'form-control '.$error_class]) !!}>
+    @if($placeholder)
+        <option selected value="" @if($placeholderDisabled) disabled @endif }}>{{$placeholderText}}</option>
+    @endif
     {{$slot}}
 </select>
-{{--</span>--}}
 @include('form::components.footer')
-@push('js')
-    <link rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/@ttskch/select2-bootstrap4-theme@x.x.x/dist/select2-bootstrap4.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-    <script>
-        $(document).ready(function () {
-            $('#{{$select_id}}').select2(
-                {
-                    theme: 'bootstrap4',
-                    placeholder: '{{$placeholder}}',
-                    allowClear: false
-                }
-            );
-            $('#{{$select_id}}').on('change', function (e) {
-                var data = $('#{{$select_id}}').select2("val");
-                @this.
-                set('{{$model}}', data);
-            });
-        });
-    </script>
-@endpush
+
+
 
 
 
