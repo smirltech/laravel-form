f58308405fac58ad7941322ab8e1437566cfd050
Marien Mupenda <marien.mupenda@gmail.com>
2023-02-02 05:07:37 +0200

    add select defaut and selectize

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -16,30 +16,36 @@
 @endphp
 @include('form::components.label')
 <span wire:ignore>
-<select id="{{$select_id}}" {!! $attributes->merge(['class' => 'form-control form-select '.$error_class]) !!}>
+<select wire id="{{$select_id}}" {!! $attributes->merge(['class' => 'form-control form-select '.$error_class]) !!}>
     {{$slot}}
 </select>
 </span>
 @include('form::components.footer')
 @push('js')
     <link rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/@ttskch/select2-bootstrap4-theme@x.x.x/dist/select2-bootstrap4.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+          href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.15.2/css/selectize.bootstrap5.css"
+          integrity="sha512-pZE3NzBgokXUM9YLBGQIw99omcxiRdkMhZkz0o7g0VjC0tCFlBUqbcLKUuX8+jfsZgiZNIWFiLuZpLxXoxi53w=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
+
+    <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.15.2/js/selectize.min.js"
+        integrity="sha512-IOebNkvA/HZjMM7MxL0NYeLYEalloZ8ckak+NDtOViP7oiYzG5vn6WVXyrJDiJPhl4yRdmNAG49iuLmhkUdVsQ=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer"
+    ></script>
     <script>
-        $(document).ready(function () {
-            $('#{{$select_id}}').select2(
-                {
-                    theme: 'bootstrap4',
-                    placeholder: '{{$placeholder}}',
-                    allowClear: false
-                }
-            );
-            $('#{{$select_id}}').on('change', function (e) {
-                var data = $('#{{$select_id}}').select2("val");
-                @this.
-                set('{{$model}}', data);
+        $(function () {
+
+            $("#{{$select_id}}").selectize({
+                plugins: ["restore_on_backspace", "clear_button"],
+                delimiter: " - ",
+                persist: false,
+                onChange: function (value) {
+                    @this.
+                    set('{{$model}}', value);
+                },
             });
+
         });
     </script>
 @endpush
