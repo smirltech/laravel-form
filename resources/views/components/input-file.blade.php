aff947f093220b1aa772a407b8306f567068913a
Marien Mupenda <marien.mupenda@gmail.com>
2023-02-08 07:15:12 +0200

    add lit of files to input-file

--- resources/views/components/input-file.blade.php
+++ resources/views/components/input-file.blade.php
@@ -1,7 +1,13 @@
-@props(['label'=>null])
+@props(['label'=>null,'media'=>null,'deleteFiles'=>false])
 <x-form::input
     label="{{ $label }}"
     type="file"
     {{ $attributes }}
 />
+
+@if($media)
+    <x-form::list-files :media="$media" :delete="$deleteFiles"/>
+@endif
+
 @include('form::components.upload-feedback')
+
