fc9665087598c55e6c1591a9c84d878f22942b68
Marien Mupenda <marien.mupenda@gmail.com>
2023-02-08 07:47:08 +0200

    add icon to button

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,6 +1,9 @@
-@props(['disabled' => false])
+@props(['disabled' => false,'icon'=>null])
 
 <button {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'btn']) !!}>
+    @if($icon)
+        <i class="{{$icon}}"></i>
+    @endif
     {{ $slot }}
     <x-form::loading target="submit"/>
 </button>
