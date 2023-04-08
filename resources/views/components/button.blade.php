a2a5db38081bcf63dd8fd755e726f9bfe697f66f
Marien Mupenda <marien.mupenda@gmail.com>
2023-04-08 09:41:04 +0200

    add label and lock defaul icon on fontawesome

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,11 +1,13 @@
-@props(['disabled' => false,'icon'=>null])
+@props(['disabled' => false,'icon'=>null,'label'=>null,'target'=>null])
 
 <button {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'btn']) !!}>
     @if($icon)
-        <i class="{{$icon}}"></i>
+        <i class="fa fa-{{$icon}}"></i>
     @endif
-    {{ $slot }}
-    <x-form::loading target="submit"/>
+    {{ $label??$slot }}
+    @if($target)
+        <x-form::loading target="{{$target}}"/>
+    @endif
 </button>
 
 
