f9dd390bc2522b9befc9ed7154cd01bd906c35d3
Marien Mupenda <marien.mupenda@gmail.com>
2023-04-17 18:25:58 +0200

    make laravel modals wok on livewire modals

---
+++ resources/views/components/scripts.blade.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.15.2/css/selectize.bootstrap5.css"
+      integrity="sha512-pZE3NzBgokXUM9YLBGQIw99omcxiRdkMhZkz0o7g0VjC0tCFlBUqbcLKUuX8+jfsZgiZNIWFiLuZpLxXoxi53w=="
+      crossorigin="anonymous" referrerpolicy="no-referrer"/>
+
+<script
+    src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.15.2/js/selectize.min.js"
+    integrity="sha512-IOebNkvA/HZjMM7MxL0NYeLYEalloZ8ckak+NDtOViP7oiYzG5vn6WVXyrJDiJPhl4yRdmNAG49iuLmhkUdVsQ=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+></script>
--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -47,44 +47,29 @@
     @endif
 </select>
 </span>
+    <!-- Start Selectize  #{{$id}} -->
+    <script>
+        $(function () {
+            $("#{{$id}}").selectize({
+                plugins: ["restore_on_backspace", "clear_button"],
+                delimiter: " - ",
+                persist: false,
+                hideSelected: true,
+                closeAfterSelect: true,
+                selectOnTab: true,
+                setFirstOptionActive: true,
+                placeholder: '{{$placeholder ?? 'Choisir '.$label ?? ''}}',
+                onChange: function (value) {
+                    @if($attributes->wire('model')->value())
+                    @this.
+                    set('{{$model}}', value);
+                    @endif
+                },
+            });
+        });
+    </script>
+    <!-- End Selectize  #{{$id}} -->
 @endif
 @include('form::components.footer')
-@if(!$refresh)
-    @push('js')
-        <!-- Start Selectize  #{{$id}} -->
-        <link rel="stylesheet"
-              href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.15.2/css/selectize.bootstrap5.css"
-              integrity="sha512-pZE3NzBgokXUM9YLBGQIw99omcxiRdkMhZkz0o7g0VjC0tCFlBUqbcLKUuX8+jfsZgiZNIWFiLuZpLxXoxi53w=="
-              crossorigin="anonymous" referrerpolicy="no-referrer"/>
-
-        <script
-            src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.15.2/js/selectize.min.js"
-            integrity="sha512-IOebNkvA/HZjMM7MxL0NYeLYEalloZ8ckak+NDtOViP7oiYzG5vn6WVXyrJDiJPhl4yRdmNAG49iuLmhkUdVsQ=="
-            crossorigin="anonymous"
-            referrerpolicy="no-referrer"
-        ></script>
-        <script>
-            $(function () {
-                $("#{{$id}}").selectize({
-                    plugins: ["restore_on_backspace", "clear_button"],
-                    delimiter: " - ",
-                    persist: false,
-                    hideSelected: true,
-                    closeAfterSelect: true,
-                    selectOnTab: true,
-                    setFirstOptionActive: true,
-                    placeholder: '{{$placeholder ?? 'Choisir '.$label ?? ''}}',
-                    onChange: function (value) {
-                        @if($attributes->wire('model')->value())
-                        @this.
-                        set('{{$model}}', value);
-                        @endif
-                    },
-                });
-            });
-        </script>
-        <!-- End Selectize  #{{$id}} -->
-    @endpush
-@endif
 
 
