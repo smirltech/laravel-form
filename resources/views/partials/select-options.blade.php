2644852c55e2e2faf69b0c2f95a0e35f63bfd94a
Marien Mupenda <marien.mupenda@gmail.com>
2023-05-19 23:13:52 +0200

    call label only if exists

--- resources/views/partials/select-options.blade.php
+++ resources/views/partials/select-options.blade.php
@@ -1,6 +1,6 @@
 <option value=""></option>
 @foreach($options as $option)
     <option
-        value="{{$option->value??$option->id??'option'.$loop->iteration}}">{{$option->label??$option->label()??$option->name??$option->nom??$option->id??'Option '.$loop->iteration}}</option>
+        value="{{$option->value??$option->id??'option'.$loop->iteration}}">{{$option->label??(method_exists($option,'label')?$option->label():null)??$option->name??$option->nom??$option->id??'Option '.$loop->iteration}}</option>
 @endforeach
 {{$slot}}
