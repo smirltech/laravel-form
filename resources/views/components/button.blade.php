cd618f66ae4964f36d1b39a3a192e8140109fc5f
Marien Mupenda <marien.mupenda@gmail.com>
2023-05-30 21:06:52 +0200

    add link, sm and size attributes

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,14 +1,33 @@
-@props(['disabled' => false,'icon'=>null,'label'=>null,'target'=>'submit','theme'=>'primary'])
+@props([
+    'disabled' => false,
+    'icon'=>null,
+    'label'=>null,
+    'target'=>'submit',
+    'theme'=>'primary',
+    'link'=>null,
+    'size'=>'md',
+    'sm'=>false,
+    ])
 
-<button {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'btn btn-'.$theme]) !!}>
-    @if($icon)
-        <i class="fa fa-{{$icon}}"></i>
-    @endif
-    {{ $label??$slot }}
-    @if($target and $attributes['type']=='submit')
-        <x-form::loading target="{{$target}}"/>
-    @endif
-</button>
+@if($sm)
+    @php($size='sm')
+@endif
+
+
+@if($link)
+    <a href="{{$link}}">
+        <button {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => "btn btn-{$theme} btn-{$size}"]) !!}>
+            @if($icon)
+                <i class="fa fa-{{$icon}}"></i>
+            @endif
+            {{ $label??$slot }}
+            @if($target and $attributes['type']=='submit')
+                <x-form::loading target="{{$target}}"/>
+            @endif
+        </button>
+    </a>
+@endif
+
 
 
 
