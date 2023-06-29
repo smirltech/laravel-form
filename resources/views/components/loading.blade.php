9b121fcbee318b2d14cadac30c7354921ff4d571
Marien Mupenda <marien.mupenda@gmail.com>
2023-06-29 18:19:31 +0200

    fix target on wire loading

--- resources/views/components/loading.blade.php
+++ resources/views/components/loading.blade.php
@@ -1,4 +1,6 @@
 @props(['target'])
-<span wire:loading wire:target="{{$target??''}}" class="ml-2">
+@if($target)
+    <span wire:loading wire:target="{{$target}}" class="ml-2">
     <i class="spinner-border spinner-border-sm"></i>
 </span>
+@endif
