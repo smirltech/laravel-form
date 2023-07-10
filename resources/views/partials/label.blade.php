9aaac471e56ff8e53be670bfc4fa81d3f2c8d349
Marien Mupenda <marien.mupenda@gmail.com>
2023-07-10 18:30:39 +0200

    add lang fr and fix

--- resources/views/partials/label.blade.php
+++ resources/views/partials/label.blade.php
@@ -1,7 +1,7 @@
 @if(!empty($label))
     <label class="form-label">{{$label}}
         @if($attributes->has('required'))
-            <span title="{{ __('laravel-form.field.required') }}" class="text-danger">*</span>
+            <span title="{{ __('form::laravel-form.field.required') }}" class="text-danger">*</span>
         @endif
     </label>
 @endif
